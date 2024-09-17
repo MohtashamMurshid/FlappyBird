56bbc250365d4e8eab601feb15ed64af4a7fd780
MohtashamMurshid <154406804+MohtashamMurshid@users.noreply.github.com>
2024-09-17 15:57:58 +0530

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int boardWidth = 360;
+        int boardHeight = 640;
+        JFrame frame = new JFrame("Flappy Bird");
+
+        // Set frame size before setting it visible
+        frame.setSize(boardWidth, boardHeight);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Add your panel (FlappyBird)
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+
+        // Center the frame on the screen
+        frame.setLocationRelativeTo(null);
+
+        // Finally, make the frame visible
+        frame.setVisible(true);
     }
 }
